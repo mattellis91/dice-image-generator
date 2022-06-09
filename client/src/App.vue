fc2589b4a4e00398b449f4774c44bdc82c33df51
Matt Ellis <mathew.ellis2@griffithuni.edu.au>
2022-06-09 14:55:48 +1000

    added vue client skeleton

---
+++ client/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink> -->
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/base.css';
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import axios from 'axios';
+
+onMounted(() => {
+  console.log("mounted");
+  axios.get("http://localhost:5000/").then((res) => {
+    console.log(res);
+  });
+});
+</script>
+
+<template>
+  <main>
+    <div>test</div>
+  </main>
+</template>
